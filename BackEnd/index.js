5a76e4b0b1138b9b8e33063adcf8b368d8893253
XuperNova97 <47016925+c1029324620@users.noreply.github.com>
2022-07-12 01:24:49 -0700

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -8,16 +8,12 @@ const app = express();
 const port = 3001;
 const host = "localhost"
 const API_BASE_URL = "https://api.tiingo.com/tiingo/utilities/search";
-const API_KEY_VALUE = "b6b0f3b6233249f339f26db0764b9df0b6b7af29";
+const API_KEY_VALUE = "replace with your own API key";  //replace this with your own tiingo API key
 const API_DAILY_URL = "https://api.tiingo.com/tiingo/daily/";
 const API_IEX_URL = "https://api.tiingo.com/iex/";
 
-const NEWS_API_KEY = "6ec527968d404e808dde312ea3c2c2e5";
+const NEWS_API_KEY = "replace with your own API key"; //replace this with your own NewsApi key 
 const API_NEWS_URL = "https://newsapi.org/v2/everything";
-/*https://api.tiingo.com/tiingo/utilities/search?query=aapl&token=b6b0f3b6233249f339f26db0764b9df0b6b7af29
-https://api.tiingo.com/iex/?tickers=<ticker>&token=<APIKeyTiingo>
-https://newsapi.org/v2/everything?q=<ticker>&from=2022-06-08&sortBy=popularity&apiKey=API_KEY
-*/
 app.use(cors())
 
 app.all('*', function (req, res, next) {
@@ -27,31 +23,6 @@ app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Credentials", true);
     next();
   });
-
-
-/*app.use('/search', proxy(API_BASE_URL, {
-    forwardPath: function(req, res) {
-        API_BASE_URL = `${API_BASE_URL}?query=${req.query.query}&token=${API_KEY_VALUE}`
-    }
-}))*/
-//?query=apple&token=${API_KEY_VALUE}
-
-//500 Internal Server Error
-/*
-app.get('/search', function (req, res, next) {
-    API_SERVICE_URL = `${API_BASE_URL}?query=${req.query.query}&token=${API_KEY_VALUE}`,
-    next(
-        createProxyMiddleware({
-            target: API_SERVICE_URL,
-            changeOrigin: true,
-            pathRewrite: {
-                '^/search':"",
-            }
-        })
-    )
-})*/
-
-//稍微可行的
 
 app.use(
     '/news',
@@ -71,7 +42,7 @@ app.use(
     '/iex/',
         createProxyMiddleware({
             target:API_IEX_URL,
-            auth: 'c1029324620:cgg2022',
+            auth: 'username:password',  //your username and password
             changeOrigin: true,
             pathRewrite: {
                 '^/iex/': "",
@@ -83,7 +54,7 @@ app.use(
     '/daily/', 
         createProxyMiddleware ({
             target: API_DAILY_URL,
-            auth: 'c1029324620:cgg2022',
+            auth: 'username:password', //your username and password
             changeOrigin:true,
             pathRewrite: {
                 '^/daily/': "",
@@ -95,8 +66,7 @@ app.use(
     '/search',
         createProxyMiddleware ({
             target: API_BASE_URL,
-            auth: 'c1029324620:cgg2022',
-            //auth: 'Token b6b0f3b6233249f339f26db0764b9df0b6b7af29',
+            auth: 'username:password', //your username and password
             changeOrigin: true,
             pathRewrite: {
                 '^/search': "",
@@ -109,17 +79,3 @@ app.use(
 app.listen(port, host, () => {
     console.log(`starting proxy at ${host}:${port}`)
 })
-
-/*
-app.use(async (req, res) => {
-    let resp = await axios ({
-        baseURL: 'https://api.tiingo.com/',
-        method: 'get',
-        url:'/api/tiingo/utilities/search/apple'
-    })
-    res.send(resp.data)
-})
-
-app.listen(port, () => {
-    console.log(`listening on localhost: ${port}`)
-})*/
